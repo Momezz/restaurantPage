20cab77d30308cf9fb89aa44190ed9f66d421cf9
Julian Gomez <julgomez14@gmail.com>
2023-02-02 23:40:20 -0500

    redux toolkit configuration and testing

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,21 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
+  // </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
