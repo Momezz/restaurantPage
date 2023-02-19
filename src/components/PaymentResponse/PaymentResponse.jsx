1935a724667eafda7f89d9e4b1bf04f2948da6e8
Julian Gomez <julgomez14@gmail.com>
2023-02-19 12:22:32 -0500

    component that communicates payment status

---
+++ src/components/PaymentResponse/PaymentResponse.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import './styles.css';
+
+const PaymentResponse = ({ statePayment }) => (
+  <article className="payment-response__container">
+    {statePayment
+      ? <h2>Payment made successfully, Thanks for your purchase</h2>
+      : <p>We have problems with your payment. Please check the data entered</p>}
+  </article>
+);
+
+PaymentResponse.propTypes = {
+  statePayment: PropTypes.bool.isRequired,
+};
+
+export default PaymentResponse;
